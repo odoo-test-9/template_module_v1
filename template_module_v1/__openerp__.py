adc36e151aeb0871a8ee622b6bc7824e35f0de7f
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2017-01-22 23:01:14 -0300

    1
    1

---
+++ template_module_v1/__openerp__.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+{
+    'name': 'Ejemplo de módulo - mfh - Accounting',
+    'version': '1.1',
+    'author': 'Marlon Falcón Hernandez',
+    'category': 'mfh',
+    'description': """
+Ejemplo de un módulo
+==========================
+Descripción del módulo
+    * Texto 01
+    * Texto 02
+""",
+    'website': 'http://www.marlonfalcon.cl',
+    'depends': ['account', 'l10n_multilang'],
+    'data': [
+        #'account.chart.template.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
